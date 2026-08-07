935b0436e4c87a3c9e5c41457cac4efa8647df4c
Kunal Yadav <24.a09.031@gmail.com>
2026-08-07 23:48:06 +0530

    Print right angled triangle pattern
    Implemented a right angled triangle pattern using nested loops. Practiced increasing the number of characters in each row while improving understanding of nested iteration and pattern logic.

---
+++ 02-Patterns/06_right_triangle.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int height;
+    char ch;
+
+    cout << "Enter height of the triangle: ";
+    cin >> height;
+
+    cout << "Enter the symbol: ";
+    cin >> ch;
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
