a6d81c8383a0e3a98d2e1a4b783a3519421b8448
Kunal Yadav <24.a09.031@gmail.com>
2026-08-09 23:14:28 +0530

    Print number pyramid pattern
    Implemented a number pyramid pattern using nested loops. Practiced controlling spaces, increasing numbers, and decreasing numbers to create a symmetric pyramid structure in C++.

---
+++ 02-Patterns/14_number_pyramid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int height;
+
+    cout << "Enter height of the pyramid: ";
+    cin >> height;
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int k = 1; k <= height - i; k++)
+        {
+            cout << "  ";
+        }
+
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+
+        for (int l = i - 1; l >= 1; l--)
+        {
+            cout << l << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
